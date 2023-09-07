519d25a7bb5c9ab89b845b21d75698ae8e880fd9
Aaron Ho <hoyanxun@gmail.com>
2023-09-07 15:52:11 +0800

    finished main menu error catching

--- HO_YAN_XUN_TP073669.py
+++ HO_YAN_XUN_TP073669.py
@@ -4,30 +4,39 @@
 # Check if the program is running the first time
 # Checks by detecting the existence of ppe.txt and other files
 def initCheck():
+    isFirst = False
     try:
         with open('ppe.txt', "r") as file:
             print(file)
     except FileNotFoundError:
-        print("File not found")
-    
-    isFirst = False
+        print("Initializing system...")
+        isFirst = True
 
     if isFirst:
         initialization()
 
 # Creates ppe.txt and propmts user to input the items
 def initialization():
-    pass
+    print("initialization complete.")
 
 def loginMenu():
     pass
 
 def mainMenu():
-    pass
+    choice = input()
+    try:
+        int(choice)
+    except:
+        print("Error, pls try again")
+    else:
+        match int(choice):
+            case 5:
+                quit()
 
 def main():
     initCheck()
-    pass
+    while True:
+        mainMenu()
 
 if __name__ == "__main__":
     main()
