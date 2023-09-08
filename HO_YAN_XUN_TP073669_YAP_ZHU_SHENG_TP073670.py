8323cc9a8522364271c3a84ed60d344e5e788657
Aaron Ho <hoyanxun@gmail.com>
2023-09-08 14:49:50 +0800

    Minor update

--- HO_YAN_XUN_TP073669_YAP_ZHU_SHENG_TP073670.py
+++ HO_YAN_XUN_TP073669_YAP_ZHU_SHENG_TP073670.py
@@ -1,8 +1,5 @@
-# HO YAN XUN
-# TP073669
-
-# YAP ZHU SHENG
-# TP073670
+# HO YAN XUN YAP ZHU SHENG
+# TP073669 TP073670
 
 # Check if the program is running the first time
 # Checks by detecting the existence of ppe.txt and other files
